9aed90b9f1c32c48d4d6dc53a1761690610b0ddb
AK_ak <ak@kali.ak>
2026-01-03 20:15:50 +0300

    Fix MainActivity edge-to-edge error and add dependencies

--- src-tauri/gen/android/app/src/main/java/com/abujafar/fatora/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/abujafar/fatora/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.abujafar.fatora
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import app.tauri.android.TauriActivity
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    // لا تضف أي شيء هنا، دع Tauri يقوم بعمله
   }
 }
